27572d33b76aac8042265987c67f1ba7e946e17b
eliezer387 <pe.eliezer@gmail.com>
2025-07-18 21:48:04 +0000

    update config

--- api/firebase.js
+++ api/firebase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase';
 
 
 const config = {
-    apiKey: "",
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "goalcoach-333bb.firebaseapp.com",
     databaseURL: "https://goalcoach-333bb.firebaseio.com",
     projectId: "goalcoach-333bb",
